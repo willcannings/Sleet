d2f45e8dbed0de0f58a6e020258542cc8281391b
Will <me@willcannings.com>
2023-04-18 10:15:26 +1000

    Initial Commit

---
+++ Sleet/SleetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleetApp.swift
+//  Sleet
+//
+//  Created by Will on 18/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct SleetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
